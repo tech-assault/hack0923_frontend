1b5a157ee1414c43614f13bdc9333f2025565ed0
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-09-30 14:49:49 +0300

    feature: to add select

--- src/pages/Mall/Mall.tsx
+++ src/pages/Mall/Mall.tsx
@@ -1,56 +1,69 @@
-import { FC } from 'react'
+import { FC, useState } from "react";
 
 const Mall: FC = () => {
+  const [selectedCity, setSelectedCity] = useState<string>("");
 
-      function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        // const userEmail = email;
-        // const userPassword = password;
-        // handleLogin(userEmail, userPassword);
-      }
+  function handleCityChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedCity(e.target.value);
+  }
 
-    return (
-<div>
-    <div className="login">
-      <p className="login__welcome">Выбор ТК</p>
-      <form
-        onSubmit={handleSubmit}
-        id="login__form"
-        className="login__form"
-        noValidate
-      >
-        <p className='login__title-input'>Город</p>
-        <input
-          type="text"
-          className="login__info login__info_form_title"
-          id="title-input"
-          placeholder="Москва"
-        //   minLength="6"
-        //   maxLength="40"
-        //   onChange={handleEmailChange}
-          required
-        />
-        <span className="span title-input-error"></span>
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    // handleLogin(userEmail, userPassword);
+  }
 
-        <p className='login__title-input'>Id торгового комплекса</p>
-        <input
-          type="password"
-          className="login__info login__info_form_subtitle"
-          id="subtitle-input"
-          placeholder="aa12bbb345"
-        //   minLength="6"
-        //   maxLength="200"
-        //   onChange={handlePasswordChange}
-          required
-        />
-        <span className="span subtitle-input-error"></span>
-        <button type="submit" className="login__button-save">
-        Перейти к данным
-        </button>
-      </form>
+  return (
+    <div>
+      <div className="login">
+        <p className="login__welcome">Выбор ТК</p>
+        <form
+          onSubmit={handleSubmit}
+          id="login__form"
+          className="login__form"
+          noValidate
+        >
+          <p className="login__title-input">Город</p>
+          <select
+            size={1}
+            value={selectedCity}
+            onChange={handleCityChange}
+            className="login__info login__info_form_title"
+            required
+          >
+            <option value="Москва">Москва</option>
+            <option value="Санкт-Петербург">Санкт-Петербург</option>
+            <option value="Саранск">Саранск</option>
+            <option value="Екатеринбург">Екатеринбург</option>
+            <option value="Пермь">Пермь</option>
+            <option value="Ростов-на-дону">Ростов-на-дону</option>
+            <option value="Самара">Самара</option>
+            <option value="Владимир">Владимир</option>
+            <option value="Нижний Новгород">Нижний Новгород</option>
+            <option value="Пенза">Пенза</option>
+            <option value="Петрозаводск">Петрозаводск</option>
+            <option value="Мурманск">Мурманск</option>
+          </select>
+          <span className="span title-input-error"></span>
+
+          <p className="login__title-input">Id торгового комплекса</p>
+          <input
+            type="password"
+            className="login__info login__info_form_subtitle"
+            id="subtitle-input"
+            placeholder="aa12bbb345"
+            //   minLength="6"
+            //   maxLength="200"
+            //   onChange={handlePasswordChange}
+            required
+          />
+          <span className="span subtitle-input-error"></span>
+          <button type="submit" className="login__button-save">
+            Перейти к данным
+          </button>
+        </form>
+      </div>
     </div>
-</div>
-    )
-}
+  );
+};
 
 export default Mall;
