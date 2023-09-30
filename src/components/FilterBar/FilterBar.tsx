36868718523a42112ef601809296a7acc97e385e
Artem Shamsiev <shamsievartyom@gmail.com>
2023-09-30 18:49:02 +0600

    feature: add lenta logo

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from './FilterBar.module.css'
+import lenta_logo from '../../vendor/images/lenta_logo.svg'
 
 const FilterBar = () => {
   return (
-    <section className={styles.section}></section>
+    <section className={styles.section}>
+        <img src={lenta_logo} className={styles.logo} alt="Лента лого" />
+    </section>
   )
 }
 
