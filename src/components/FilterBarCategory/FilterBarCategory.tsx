31fc5e760d07f5a2f8153c0b731dcca718dfc6c0
Artem Shamsiev <shamsievartyom@gmail.com>
2023-10-03 00:17:36 +0600

    feature: working search in FilterBar

--- src/components/FilterBarCategory/FilterBarCategory.tsx
+++ src/components/FilterBarCategory/FilterBarCategory.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
 import styles from './FilterBarCategory.module.css'
 import icon_arrow_down from './../../vendor/images/icon_arrow_down.svg'
 import icon_arrow_right from './../../vendor/images/icon_arrow_right.svg'
@@ -12,14 +12,27 @@ type FilterBarCategoryProps = {
 
 const FilterBarCategory: FC<FilterBarCategoryProps> = ({ title, openedFilter, setOpenedFilter }) => {
 
+    //DELETE THIS STRING
+    const TEST_DATA = ['ТК 1', 'ТК 2', 'ТК 3', 'ТК 4', 'ТК 5', 'ТК 6', 'ТК 7', 'ТК 8', 'ТК 9', 'ТК 10', 'ТК 11',]
+    //DELETE THIS STRING  
+
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredData, setFilteredData] = useState(TEST_DATA);
+
     function handleClick() {
         if (openedFilter !== title) setOpenedFilter(title)
         else setOpenedFilter(null)
     }
 
-    //DELETE THIS STRING
-    const TEST_DATA = ['ТК 1', 'ТК 2', 'ТК 3', 'ТК 4', 'ТК 5', 'ТК 6', 'ТК 7', 'ТК 8', 'ТК 9', 'ТК 10', 'ТК 11',]
-    //DELETE THIS STRING  
+    useEffect(() => {//search
+        if (searchValue !== '') {//Show filtered list or initial data if string=''
+            const newFilteredData = TEST_DATA.filter(el => el.toLowerCase().includes(searchValue.toLowerCase()));
+            setFilteredData(newFilteredData);
+        } else {
+            setFilteredData(TEST_DATA);
+        }
+    }, [searchValue]);
+
     return (
         <div className={styles.container}>
             <button className={`${styles.button} ${openedFilter === title ? styles.button_selected : ''}`} onClick={handleClick}>
@@ -32,9 +45,9 @@ const FilterBarCategory: FC<FilterBarCategoryProps> = ({ title, openedFilter, se
             </button>
             {openedFilter === title && (
                 <>
-                    <input className={styles.search} type="text" placeholder='Поиск...' />
+                    <input className={styles.search} type="text" placeholder='Поиск...' onChange={e => setSearchValue(e.target.value)} />
                     <ul className={styles.list}>
-                        {TEST_DATA.map((el, index) => {
+                        {filteredData.map((el, index) => {
                             return (
                                 <li className={styles.list_item} key={index}>
                                     <label className={styles.input_label} htmlFor={`checkbox-${index}`}>
