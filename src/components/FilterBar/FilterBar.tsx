5287d645c85529ad415b45224ecb2a5913b163a5
Artem Shamsiev <shamsievartyom@gmail.com>
2023-10-02 22:53:05 +0600

    feature: update styles FilterBar

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from './FilterBar.module.css'
 import lenta_logo from '../../vendor/images/lenta_logo.svg'
 import FilterBarCategory from '../FilterBarCategory/FilterBarCategory'
 
+export type filterType = 'ТК' | 'Группа' | 'Категория' | 'Подкатегория' | 'Товар' | null
+
 const FilterBar = () => {
+
+  const [openedFilter, setOpenedFilter] = useState<filterType>('ТК')
 
   return (
     <section className={styles.section}>
-        <img src={lenta_logo} className={styles.logo} alt="Лента лого" />
-        <FilterBarCategory title='ТК'/>        
+      <img src={lenta_logo} className={styles.logo} alt="Лента лого" />
+      <ul className={styles.list}>
+        <li><FilterBarCategory title='ТК' openedFilter={openedFilter} setOpenedFilter={setOpenedFilter} /></li>
+        <li><FilterBarCategory title='Группа' openedFilter={openedFilter} setOpenedFilter={setOpenedFilter} /></li>
+        <li><FilterBarCategory title='Категория' openedFilter={openedFilter} setOpenedFilter={setOpenedFilter} /></li>
+        <li><FilterBarCategory title='Подкатегория' openedFilter={openedFilter} setOpenedFilter={setOpenedFilter} /></li>
+        <li><FilterBarCategory title='Товар' openedFilter={openedFilter} setOpenedFilter={setOpenedFilter} /></li>
+      </ul>
     </section>
   )
 }
--- src/components/FilterBarCategory/FilterBarCategory.tsx
+++ src/components/FilterBarCategory/FilterBarCategory.tsx
@@ -1,30 +1,46 @@
-import { useState, FC } from 'react'
+import { FC } from 'react'
 import styles from './FilterBarCategory.module.css'
+import icon_arrow_down from './../../vendor/images/icon_arrow_down.svg'
+import icon_arrow_right from './../../vendor/images/icon_arrow_right.svg'
+import { filterType } from '../FilterBar/FilterBar';
 
 type FilterBarCategoryProps = {
-    title: string;
+    title: filterType;
+    openedFilter: filterType;
+    setOpenedFilter: React.Dispatch<React.SetStateAction<filterType>>;
 }
 
-const FilterBarCategory: FC<FilterBarCategoryProps> = ({ title }) => {
-    const [isOpen, setIsOpen] = useState(false);
+const FilterBarCategory: FC<FilterBarCategoryProps> = ({ title, openedFilter, setOpenedFilter }) => {
 
-    const toggleOpen = () => setIsOpen(!isOpen);
+    function handleClick() {
+        if (openedFilter !== title) setOpenedFilter(title)
+        else setOpenedFilter(null)
+    }
 
     //DELETE THIS STRING
     const TEST_DATA = ['ТК 1', 'ТК 2', 'ТК 3', 'ТК 4', 'ТК 5', 'ТК 6', 'ТК 7', 'ТК 8', 'ТК 9', 'ТК 10', 'ТК 11',]
     //DELETE THIS STRING  
     return (
         <div className={styles.container}>
-            <button className={styles.button} onClick={toggleOpen}>{title}</button>
-            {isOpen && (
+            <button className={`${styles.button} ${openedFilter === title ? styles.button_selected : ''}`} onClick={handleClick}>
+                {title}
+                {openedFilter === title ? (//arrow icon direction
+                    <img src={icon_arrow_down} />
+                ) : (
+                    <img src={icon_arrow_right} />
+                )}
+            </button>
+            {openedFilter === title && (
                 <>
-                    <input type="text" placeholder='Поиск...' />
+                    <input className={styles.search} type="text" placeholder='Поиск...' />
                     <ul className={styles.list}>
                         {TEST_DATA.map((el, index) => {
                             return (
                                 <li className={styles.list_item} key={index}>
-                                    <input className={styles.input_checkbox} type='checkbox' id={`checkbox-${index}`} />
-                                    <label className={styles.input_label} htmlFor={`checkbox-${index}`}>{el}</label>
+                                    <label className={styles.input_label} htmlFor={`checkbox-${index}`}>
+                                        <input className={styles.input_checkbox} type='checkbox' id={`checkbox-${index}`} />
+                                        {el}
+                                    </label>
                                 </li>
                             )
                         })}
