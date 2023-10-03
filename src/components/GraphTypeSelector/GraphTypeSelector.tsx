cc71ebf94c5e271430f55d5a59240186ef0c5934
Artem Shamsiev <shamsievartyom@gmail.com>
2023-10-03 17:48:00 +0600

    feature: add header

---
+++ src/components/GraphTypeSelector/GraphTypeSelector.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import styles from './GraphTypeSelector.module.css'
+
+const GraphTypeSelector = () => {
+
+    const [selectedButton, setSelectedButton] = useState(0)
+
+    function handleClick(number: number) {
+        setSelectedButton(number)
+    }
+
+    return (
+        <ul className={styles.list}>
+            <li>
+                <button className={`${styles.button} ${selectedButton === 0 && styles.button_selected}`} onClick={() => handleClick(0)}>Прогноз спроса</button>
+            </li>
+            <li>
+                <button className={`${styles.button} ${selectedButton === 1 && styles.button_selected}`} onClick={() => handleClick(1)}>Качество спроса</button>
+            </li>
+        </ul>
+    )
+}
+
+export default GraphTypeSelector
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react'
 import styles from './MainPage.module.css'
 import FilterBar from '../../components/FilterBar/FilterBar'
+import GraphTypeSelector from '../../components/GraphTypeSelector/GraphTypeSelector'
+import { Link } from 'react-router-dom'
+import icon_logout from './../../vendor/images/icon_logout.svg'
 
 const MainPage: FC = () => {
     return (
         <main className={styles.container}>
             <FilterBar />
             <div className={styles.content}>
-                
+                <div className={styles.header}>
+                    <GraphTypeSelector />
+                    <Link className={styles.logout} to='./login'>
+                        <img src={icon_logout} />
+                    </Link>
+                </div>
             </div>
         </main>
     )
